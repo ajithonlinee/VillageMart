67833e8bc1486d5bd387a356c2ead9093512c130
Ajith Kumar <ajithonlinee@gamil.com>
2026-06-04 11:13:10 +0530

    Install whitenoise library engine and fix asset regex routing paths

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,10 +10,9 @@ urlpatterns = [
     path('api/', include('orders.urls')),
 ]
 
-# Force Django to serve static media elements using WhiteNoise route layers when DEBUG is False
 if not settings.DEBUG:
     urlpatterns += [
-        path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     ]
 else:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
